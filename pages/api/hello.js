29254b743b6e86a2b6517559b8c6079e26672695
rslijp <renzo.slijp@softcause.nl>
2021-11-09 15:18:13 +0100

    added sample code

---
+++ pages/api/hello.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const {cookies} = req;
+  res.status(200).json({cookie: cookies["demo-cookie"]})
+}
